fc78e8ff0d6c041e09e8790e6018d633e374c017
PeterV293 <p.venner@student.avans.nl>
2023-10-12 11:26:40 +0200

    added modelels and dbcontext

---
+++ Data/Models/Inventory.cs
@@ -0,0 +1,12 @@
+﻿namespace Ninja.Data.Models
+{
+    public class Inventory
+    {
+        public int UserId { get; set; }
+        public int EquipmentId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedDate { get; set; }
+        public User User { get; set; }
+        public Equipment Equipment { get; set; }
+    }
+}
