08afaf0c4f5ee56d6d59573ef1c51129c55d1e84
PeterV293 <p.venner@student.avans.nl>
2023-10-12 15:16:51 +0200

    Update Startup.cs

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -12,52 +12,26 @@ namespace Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add Entity Framework Core with a connection string
-            services.AddDbContext<NinjaEquipmentDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            // Add MVC services
-            services.AddControllers();
-
-            // Add other services, repositories, authentication, etc. as needed
-
-            // Configure your application's settings
-            // Example: services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.AddRazorPages();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (!env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
