4e1a029660df1b42a3804313c52d9b797ed3766c
PeterV293 <p.venner@student.avans.nl>
2023-10-16 20:35:15 +0200

    added db requirements

--- Data/Models/Equipment.cs
+++ Data/Models/Equipment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Data.Enum;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Data.Models;
@@ -7,10 +8,17 @@ public class Equipment
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; }
+    [Range(0, int.MaxValue)]
     public int ValueInGold { get; set; }
+    [Required]
     public string Category { get; set; }
+    [Range(int.MinValue, int.MaxValue)]
     public int Strength { get; set; }
+    [Range(int.MinValue, int.MaxValue)]
     public int Intelligence { get; set; }
+    [Range(int.MinValue, int.MaxValue)]
     public int Agility { get; set; }
 }
--- Data/Models/Ninja.cs
+++ Data/Models/Ninja.cs
@@ -7,7 +7,11 @@ public class Ninja
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; }
-    public double Gold { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)] 
+    public int Gold { get; set; }
     public List<NinjaEquipment> NinjaEquipment { get; set; }
 }
