fb53a14e9382397258bd3912bae404d0cac2ee3c
lucas <lam.kooijmans@student.avans.nl>
2023-10-18 09:25:13 +0200

    Ninja image and stuff

--- Data/Migrations/20231017081326_DoubleToInt.Designer.cs
+++ Data/Migrations/20231017081326_DoubleToInt.Designer.cs
@@ -3,6 +3,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(NinjaEquipmentDbContext))]
-    partial class NinjaEquipmentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017081326_DoubleToInt")]
+    partial class DoubleToInt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
